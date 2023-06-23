6edd73674ba36eea614a77b94832dd572a200557
佐藤太一 <3101taichi@gmail.com>
2023-06-23 21:45:58 +0900

    記事一覧ページの作成

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,40 @@
-export default function Home() {
-  return <div className="my-8">コンテンツ</div>;
+import fs from 'fs';
+import matter from 'gray-matter';
+
+import PostCard from '../components/PostCard';
+
+// TODO: 型指定する 
+type PostsProps = {
+  posts: any
+}
+
+export const getStaticProps = () => {
+  const files = fs.readdirSync('posts');
+  const posts = files.map((fileName) => {
+    const slug = fileName.replace(/\.md$/, '');
+    const fileContent = fs.readFileSync(`posts/${fileName}`, 'utf-8');
+    const { data } = matter(fileContent);
+    return {
+      frontMatter: data,
+      slug,
+    };
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
+};
+
+export default function Home({ posts }: PostsProps) {
+  return (
+    <div className="my-8">
+      <div className="grid grid-cols-3">
+        {posts.map((post: post) => (
+          <PostCard key={post.slug} post={post} />
+        ))}
+      </div>
+    </div>
+  );
 }
